25ae7ae1db14164998306607aab168ff1336e38f
harikkrishna <31917249+harikkrishna@users.noreply.github.com>
2017-09-16 21:47:06 +0530

    Add files via upload

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -1,27 +1,4 @@
 import java.util.Date;
-
-/**
- * A fix-sized array of students
- * array length should always be equal to the number of stored elements
- * after the element was removed the size of the array should be equal to the number of stored elements
- * after the element was added the size of the array should be equal to the number of stored elements
- * null elements are not allowed to be stored in the array
- * 
- * You may add new methods, fields to this class, but DO NOT RENAME any given class, interface or method
- * DO NOT PUT any classes into packages
- *
- */
-public class StudentGroup implements StudentArrayOperation {
-
-	private Student[] students;
-	
-	/**
-	 * DO NOT remove or change this constructor, it will be used during task check
-	 * @param length
-	 */
-	public StudentGroup(int length) {
-		this.students = new Student[length];
-	}import java.util.Date;
 import java.lang.Exception;
 import java.util.*;
 import java.util.ArrayList;
@@ -342,10 +319,25 @@ j++;
 
     }
 
+    @Override
+    public Student getNextStudent(Student student) {
+        // Add your implementation here
+        Student st8=null;
+        if (student == null)
+            throw new IllegalArgumentException();
+        else 
+        {
+            for (int i = 0; i < students.length; i++)
+            {
+                if (students[i].getId() == student.getId())
+                {
+                    st8 = students[i + 1];
+                    
+                }
+            }
+return st8;
+        }
+        
 
-	@Override
-	public Student getNextStudent(Student student) {
-		// Add your implementation here
-		return null;
-	}
+    }
 }
